5e9aaea9dbb86e0cbffb5331204e1011cfa06701
Gerardo <gconsuelosf@gmail.com>
2023-05-12 23:09:45 -0500

    added parser file in custom-tfrecord format folder

---
+++ src/tasks/classification/dataset/custom-tfrecord/parser.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+class Parser():
+    def __init__(self,
+                 root,              # directory for data
+                 labels,            # labels in the data
+                 batch_size):       # batch size for train and val
+        super(Parser, self).__init__()
+
+        self.root = root
+        self.labels = labels
+        self.batch_size = batch_size
+
+        print("Hi Python")
+    
--- src/tasks/classification/modules/trainer.py
+++ src/tasks/classification/modules/trainer.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import importlib
+import sys
+import __init__ as booger
 from common.logger import Logger
 
 class Trainer():
@@ -9,4 +12,17 @@ class Trainer():
         self.log_dir = log_dir
 
         self.tb_logger = Logger(self.log_dir + "/tb")
+
+        # dynamically import the parser specified in the DATA config file
+        parserModuleName = "parserModule"
+        parserSpec = importlib.util.spec_from_file_location(parserModuleName,
+                                       booger.TRAIN_PATH + '/tasks/classification/dataset/' +
+                                       self.DATA["name"] + '/parser.py')
+        parserModule = importlib.util.module_from_spec(parserSpec)
+        sys.modules[parserModuleName] = parserModule
+        parserSpec.loader.exec_module(parserModule)
+
+        self.parser = parserModule.Parser(root=self.data_dir,
+                                          labels=self.DATA["labels"],
+                                          batch_size=self.ARCH["train"]["batch_size"])
         
